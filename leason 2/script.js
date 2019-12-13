9badc494e4fcbcbb8444451dd392c89b39a31cb4
Igor <igorelllo@gmail.com>
2019-12-13 15:49:42 +0200

    first comm

---
+++ leason 2/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+{
+    let second = 2;
+}
+
+const pi = 3.14;
+
+
+
+console.log(4/0);
+console.log('string'*9);
+
+let goy;
+
+console.log(goy);
+
+let persone = {
+    name: "koto",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+let arr = ['plu.png', 'tru.jpg','apple.bpm'];
+
+console.log(arr[0]);
+
+// alert("stoping");
+
+// let answer = confirm('are you humane?');
+
+
+//  let answer = +prompt("yes?", "no");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+// console.log(4 + "- object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+
+console.log("2" === 2);
+
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
